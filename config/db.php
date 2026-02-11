b4eb608209fcb108d2a2546580dffec37f7ed3e0
Yuichiro Takahara <yuichiro.takahara1234@gmail.com>
2026-02-11 02:11:20 +0000

    modify db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,14 @@
 <?php
-$host = 'localhost';
-$db   = 'sample_app';
-$user = 'cyber';
-$pass = 'cyber00';
-$charset = 'utf8mb4';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$db   = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$pass = $_ENV['DB_PASS'];
+$charset = $_ENV['DB_CHARSET'];
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
